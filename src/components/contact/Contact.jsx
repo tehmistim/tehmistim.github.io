ebfe1dd85a712a24e1a95147eb2dd2664d60157b
Tim A. Washington <92332173+tehmistim@users.noreply.github.com>
2022-03-22 11:16:18 -0500

    updates

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_jesygsc', 'template_jy6pl88', form.current, 'tSfJj7oZ6SjCVJ11w')
+
+    e.target.reset()
+  };
+
+  return (
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>taw@tehmwashington.com</h5>
+            <a href="mailto:taw@tehmwashington.com" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>tehmistim</h5>
+            <a href="https://m.me/tehmistim" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
